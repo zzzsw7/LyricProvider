5a46cf0a5badc013cbb472d402c7a3620bf74405
zzzsw7 <104837265+zzzsw7@users.noreply.github.com>
2026-09-22 09:57:28 +0800

    Update build.gradle.kts

--- qq-music-hd/build.gradle.kts
+++ qq-music-hd/build.gradle.kts
@@ -40,7 +40,7 @@ configure<ApplicationExtension> {
             signingConfig = signingConfigs.getByName("release")
         }
         getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
+            //signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
