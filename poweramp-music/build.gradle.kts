7f3bb8f044e8e0e29f50b7d2ff06a65c89fa5001
zzzsw7 <104837265+zzzsw7@users.noreply.github.com>
2026-09-22 09:56:37 +0800

    Update build.gradle.kts

--- poweramp-music/build.gradle.kts
+++ poweramp-music/build.gradle.kts
@@ -44,7 +44,7 @@ configure<ApplicationExtension> {
             signingConfig = signingConfigs.getByName("release")
         }
         getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
+            //signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
